900d40bf6e7cd21740aedf28253b6f387ffe4861
Emilio Righi <righiemilio@gmail.com>
2024-07-02 16:00:45 +0200

    update front configs

--- client/src/router/index.ts
+++ client/src/router/index.ts
@@ -20,7 +20,7 @@ const routes: Array<RouteRecordRaw> = [
 
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHistory(import.meta.env.VITE_BASE_PATH),
   //  mode: process.env.VUE_APP_ROUTER_MODE_HISTORY === 'true' ? 'history' : 'hash',
   routes,
 })
